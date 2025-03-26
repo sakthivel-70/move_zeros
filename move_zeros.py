574eaf8d9fea0a3b07ccc5d3cb4ba5b6e65726a1
sakthivel-70 <sakthivelannamalai28@gmail.com>
2025-03-26 11:41:16 +0530

    first commit

---
+++ move_zeros.py
@@ -0,0 +1,8 @@
+def move_zeros(a):
+    b=0
+    for i in range(len(a)):
+        if a[i]!=0:
+            a[i],a[b]=a[b],a[i]
+            b+=1
+    return a
+print(move_zeros([2,0,8,0,5,0,3]))        
