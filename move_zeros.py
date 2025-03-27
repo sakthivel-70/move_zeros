7bc31a530fa3e19b976ee7a21db3f4302daa4d4a
sakthivel-70 <sakthivelannamalai28@gmail.com>
2025-03-27 13:34:34 +0530

    commented the code

--- move_zeros.py
+++ move_zeros.py
@@ -1,9 +1,14 @@
-def move_zeros(a):
+'''def move_zeros(a):
     b=0
     for i in range(len(a)):
         if a[i]!=0:
             a[i],a[b]=a[b],a[i]
             b+=1
     return a
-a=[2,63,8,0,0,8,0,7]    
+a=[4,6,0,7,0,7,8,9,0]      
+
 print(move_zeros(a))        
+'''
+a=100
+b=300
+print(a+b)
